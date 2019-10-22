4165357ee27bea5989f5d51c05346c63300ba978
alumno <alumno@aula1-10>
2019-10-22 13:56:54 +0200

    Suma de los 100 primeros numeros

---
+++ src/bucle4/bucle4.java
@@ -0,0 +1,17 @@
+package bucle4;
+
+import java.util.Scanner;
+
+public class bucle4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int suma = 0;
+		for (int i = 0; i < 101; i++) {
+			suma=suma+i;
+		}
+		System.out.println(suma);
+	}
+
+}
